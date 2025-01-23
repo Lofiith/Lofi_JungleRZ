69f193a1b9111dd802f954039d2926c078c3e9a3
Lofiith <148030987+Lofiith@users.noreply.github.com>
2025-01-22 21:00:50 -0600

    Update open.lua

--- server/open.lua
+++ server/open.lua
@@ -2,14 +2,16 @@ RegisterNetEvent('jungleRZ:onPlayerDeathServer')
 AddEventHandler('jungleRZ:onPlayerDeathServer', function(victim)
     local ped = GetPlayerPed(victim)
     if not ped or ped == 0 then return end
-
     local coords = GetEntityCoords(ped)
     for _, z in ipairs(Config.Zones) do
         if #(coords - z.coords) <= z.radius then
-            Wait(1500)
-            TriggerClientEvent('jungleRZ:teleportDead', victim, z.spawnCoords)
-            Wait(1)
-            TriggerClientEvent('esx_ambulancejob:revive', victim) -- replace your revive export if needed
+            if type(z.spawnCoords) == "table" and #z.spawnCoords > 1 then
+                local chosenSpawn = z.spawnCoords[math.random(#z.spawnCoords)]
+                TriggerClientEvent('jungleRZ:teleportDead', victim, chosenSpawn)
+            else
+                TriggerClientEvent('jungleRZ:teleportDead', victim, z.spawnCoords)
+            end
+            TriggerClientEvent('esx_ambulancejob:revive', victim) -- replace to your revive export if needed
             return
         end
     end
