c4ec85012fb08b26cbcc15e6e0e8abb0438b4f4f
Lofiith <148030987+Lofiith@users.noreply.github.com>
2025-03-06 18:51:08 -0600

    Update open.lua

--- client/open.lua
+++ client/open.lua
@@ -1,11 +1,9 @@
 RegisterNetEvent('jungleRZ:OnEnterZone', function(zone)
     DisplayRadar(false)
-    -- implement your own logic here
 end)
 
 RegisterNetEvent('jungleRZ:OnExitZone', function(zone)
     DisplayRadar(true)
-    -- implement your own logic here
 end)
 
 RegisterNetEvent('jungleRZ:teleportDead')
@@ -13,8 +11,4 @@ AddEventHandler('jungleRZ:teleportDead', function(spawnCoords)
     local ped = PlayerPedId()
     SetEntityCoordsNoOffset(ped, spawnCoords.x, spawnCoords.y, spawnCoords.z, false, false, false)
     SetEntityHeading(ped, spawnCoords.w)
-    if Config.UseInternalRevive then
-        NetworkResurrectLocalPlayer(spawnCoords.x, spawnCoords.y, spawnCoords.z, spawnCoords.w, true, false)
-        ClearPedTasksImmediately(ped)
-    end
 end)
