86ec335dcd03921da603e153d2e24d8111e3b585
Lofiith <148030987+Lofiith@users.noreply.github.com>
2025-03-06 18:44:38 -0600

    Update open.lua

--- server/open.lua
+++ server/open.lua
@@ -1,19 +1,19 @@
 RegisterNetEvent('jungleRZ:onPlayerDeathServer')
-AddEventHandler('jungleRZ:onPlayerDeathServer', function()
-    local src = source
-    local ped = GetPlayerPed(src)
-    if not ped or ped == 0 then return end
-    local coords = GetEntityCoords(ped)
-    for _, z in ipairs(Config.Zones) do
-        if #(coords - z.coords) <= z.radius then
-            if type(z.spawnCoords) == "table" and #z.spawnCoords > 1 then
-                local chosenSpawn = z.spawnCoords[math.random(#z.spawnCoords)]
-                TriggerClientEvent('jungleRZ:teleportDead', src, chosenSpawn)
-            else
-                TriggerClientEvent('jungleRZ:teleportDead', src, z.spawnCoords)
-            end
-            TriggerClientEvent('esx_ambulancejob:revive', src) -- Replace with your revive export if needed
-            return
-        end
+AddEventHandler('jungleRZ:onPlayerDeathServer', function(victim)
+    local src = victim
+    local pdata = PlayerZoneData[src]
+    if pdata then
+         local zone = pdata.zone
+         if type(zone.spawnCoords) == "table" and #zone.spawnCoords > 1 then
+             local chosenSpawn = zone.spawnCoords[math.random(#zone.spawnCoords)]
+             TriggerClientEvent('jungleRZ:teleportDead', src, chosenSpawn)
+         else
+             TriggerClientEvent('jungleRZ:teleportDead', src, zone.spawnCoords)
+         end
+         if not Config.UseInternalRevive then
+            Wait(1000)
+             TriggerClientEvent(Config.ReviveEvent, src)
+         end
+         PlayerZoneData[src] = nil
     end
 end)
