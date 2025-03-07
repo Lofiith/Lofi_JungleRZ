44b060aa1a712cf409f0a6d3e11193c26e47c780
Lofiith <148030987+Lofiith@users.noreply.github.com>
2025-03-06 18:44:28 -0600

    Update open.lua

--- client/open.lua
+++ client/open.lua
@@ -1,11 +1,11 @@
 RegisterNetEvent('jungleRZ:OnEnterZone', function(zone)
     DisplayRadar(false)
-    -- implement your hud logic if you want to hide the hud
+    -- implement your own logic here
 end)
 
 RegisterNetEvent('jungleRZ:OnExitZone', function(zone)
     DisplayRadar(true)
-    -- implement your hud logic if you want to show the hud
+    -- implement your own logic here
 end)
 
 RegisterNetEvent('jungleRZ:teleportDead')
@@ -13,4 +13,8 @@ AddEventHandler('jungleRZ:teleportDead', function(spawnCoords)
     local ped = PlayerPedId()
     SetEntityCoordsNoOffset(ped, spawnCoords.x, spawnCoords.y, spawnCoords.z, false, false, false)
     SetEntityHeading(ped, spawnCoords.w)
+    if Config.UseInternalRevive then
+        NetworkResurrectLocalPlayer(spawnCoords.x, spawnCoords.y, spawnCoords.z, spawnCoords.w, true, false)
+        ClearPedTasksImmediately(ped)
+    end
 end)
